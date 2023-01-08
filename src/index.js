fe54098f0b8cdd7a14aefb815f74cba355ded38d
Matheus Pereira <matheus.pereira@fortbrasil.com.br>
2023-01-08 20:07:45 -0300

    criando rota de saque

--- src/index.js
+++ src/index.js
@@ -47,7 +47,29 @@ app.post('/deposit', verifyIfExistAccountCpf, (request, response) => {
     }
 
     custormer.statement.push(statementOperation)
+
     return response.status(201).send();
+})
+
+app.post('/withdraw', verifyIfExistAccountCpf, (request, response) => {
+    const { amount } = request.body
+    const { customer } = request
+
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount) {
+        return response.status(400).json({ error: 'Insufficient funds!' })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
 })
 
 
